aa860a2efc717bab2fac1511870d2ba83f886f5b
Grahambone37 <electricgrunt37@gmail.com>
2022-11-26 12:25:48 -0800

    it works and so does the refresh button

--- public/index.js
+++ public/index.js
@@ -1,9 +1,13 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+    const refreshButton = document.querySelector('#refresh-button')
 
-    let countValue = 0;
+    let response = await fetch('http://localhost:9001/counter')
+    let parsedResponse = await response.json()
+    console.log(parsedResponse)
+    let countValue = parsedResponse.value;
 
     function increment(){
         countValue++;
@@ -15,8 +19,14 @@ function main(){
         countContainer.textContent = countValue;
     }
 
+    function refresh(){
+        countValue = parsedResponse.value;
+        countContainer.textContent = countValue;
+    }
+
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    refreshButton.addEventListener('click', refresh)
     countContainer.textContent = countValue;
 }
 main()
